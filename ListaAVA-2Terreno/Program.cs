073d4cc37a2835d9775a19ef0f3ff76db5273f6c
Nicoly Malescza <Nicolysilvamalescza@gmail.com>
2023-06-12 09:11:17 -0300

    Venda de terreno

---
+++ ListaAVA-2Terreno/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Informe a largura do terreno: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Informe o comprimento do terreno: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+Terreno t = new Terreno(largura,comprimento);
+
+Console.WriteLine($"A area do terreno é: {t.Area(largura,comprimento)} metros² ");
+
+Console.WriteLine("\n\nQual o valor do metro² ?");
+double metro = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"\nValor do terreno: {t.Valor(metro, largura, comprimento).ToString("C2")}");
+
+Console.ReadKey();
---
+++ ListaAVA-2Terreno/Terreno.cs
@@ -0,0 +1,42 @@
+﻿public class Terreno
+{
+    private double largura;
+    private double comprimento;
+
+    public Terreno(double largura, double comprimento)
+    {
+        this.largura = largura;
+        this.comprimento = comprimento;
+    }
+
+    public double getLargura()
+    {
+        return this.largura;
+    }
+    public void setLargura()
+    {
+        this.largura = this.largura;
+    }
+
+    public double getComprimento()
+    {
+        return this.comprimento;
+    }
+    public void setComprimento()
+    {
+        this.comprimento = this.comprimento;
+    }
+    
+    public double Area(double larg, double cumpri)
+    {
+        double conta = larg * cumpri;
+        return conta;
+    }
+    public double Valor(double metro, double l , double c)
+    {
+        double conta = l * c;
+        double calculo = metro * conta;
+        return calculo;
+    }
+
+}
